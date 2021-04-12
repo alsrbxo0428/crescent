e36f7db80c91c6e7562103b3abc0d29fab2f3450
alsrbxo0428 <alsrbxo0428@naver.com>
2021-04-12 10:29:27 +0900

    2021-04-12 마무리

---
+++ src/main/java/org/ict/service/OrderListServiceImpl.java
@@ -0,0 +1,77 @@
+package org.ict.service;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.ict.domain.CartVO;
+import org.ict.domain.OrderListVO;
+import org.ict.mapper.OrderDetailMapper;
+import org.ict.mapper.OrderListMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Service
+public class OrderListServiceImpl implements OrderListService {
+
+	@Autowired
+	private OrderListMapper mapper;
+	@Autowired
+	private OrderDetailMapper dtmapper;
+	
+	//장바구니에서 선택된 상품 정보
+	@Override
+	public List<CartVO> paymentList(@Param("cnoArr") int[] arr) {
+		return mapper.selectPayment(arr);
+	}
+	@Override
+	public List<CartVO> total(int[] arr) {
+		return mapper.total(arr);
+	}
+	//장바구니에서 선택된 상품 정보
+	
+	@Transactional
+	@Override
+	public void insertOrder(OrderListVO vo) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyMMddhhmmss");
+		Date date = new Date();
+		String str = sdf.format(date) + vo.getMno();
+		
+		vo.setOrderNumber(str);
+		
+		mapper.insertOrder(vo);
+		vo.getOrderDetail().forEach(detail -> {
+			detail.setOrderNumber(str);
+			dtmapper.insert(detail);
+		});
+	}
+	
+	@Transactional
+	@Override
+	public List<OrderListVO> selectAll(int mno) {
+		List<OrderListVO> list = mapper.selectAll(mno);
+		
+		list.forEach(item -> {
+			item.setOrderDetail(dtmapper.select(item.getOrderNumber()));
+		});
+		
+		return list;
+	}
+	
+	@Override
+	public OrderListVO selectMno(int mno) {
+		return mapper.selectMno(mno);
+	}
+	
+	@Override
+	public OrderListVO selectOrNum(String orderNumber) {
+		return mapper.selectOrNum(orderNumber);
+	}
+	
+}
